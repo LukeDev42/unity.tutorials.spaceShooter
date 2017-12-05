b07fde8aad51dde8469f33dae20446482dcab6ae
Kyle Bucholtz <bucholtz@mtsd.org>
2017-12-05 10:52:42 -0500

    Lesson 9 - Hazards
    *created an asteroid
    *created asteroid movement
    *made asteroid disappear when shot with a bolt
    *tagged boundary with new boundary tag so asteroid wouldn't disappear because of that game object

---
+++ Space Shooter/Assets/Scripts/RandomRotator1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator1 : MonoBehaviour {
+
+    public float tumble;
+    private Rigidbody astRb;
+
+    void Start()
+    {
+        astRb = GetComponent<Rigidbody>();
+        astRb.angularVelocity = Random.insideUnitSphere * tumble;
+    }
+}
